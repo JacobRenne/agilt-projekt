996c31e965588542fc0ee8330f5a15fa4b3a0fa1
Jacob Rennemark <jacobrane.se@gmail.com>
2025-02-20 19:58:26 +0100

    "Add template nav bar files"

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+
+
+</script>
+
+
+
 <template>
-  <router-view />
+
+  <NavBar/>
+
+  <router-view/>
+
 </template>
+
+
+
+<style>
+
+
+
+</style>
