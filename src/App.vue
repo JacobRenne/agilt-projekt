e77ec0f5a547a3d39afe17097997de6306f3119e
Jacob Rennemark <jacobrane.se@gmail.com>
2025-02-20 11:28:53 +0100

    Reset project to clean slate

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <script setup>
 
-import NavBar from './components/NavBar.vue';
-
 </script>
 
 <template>
- 
-	<div>
-		<NavBar/>
-	</div>
 
+  <router-view />
+  
 </template>
-
-<style>
-
-
-
-</style>
