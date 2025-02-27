729ef3d39fe426131f002d6a23e6e2d53d823a7c
Isac Ojaniemi <ebrosmc1@gmail.com>
2025-02-27 11:46:01 +0100

    fixed styling and bugg
    Fixade styling så att den är samma som orderlistan. Sen fixade bugg så att listan säger att önskelistan är tom efter den har tömts

--- src/components/WishList.vue
+++ src/components/WishList.vue
@@ -13,72 +13,29 @@ function removeFromWishList(product) {
 </script>
 
 <template>
-  <div
-    v-for="product in wishList"
-    :key="product.id"
-    class="rounded shadow-md position-relative mb-3"
-    id="bg"
-  >
-    <div class="p-4">
-      <div class="text-secondary my-2">Artikelnr: {{ product.artnr }}</div>
-      <h3 class="fs-6 fw-bold text-light">{{ product.title }}</h3>
-      <div class="row mb-2">
-        <div class="col-10">
-          <img :src="product.bild" :alt="product.title" class="img-fluid" />
-        </div>
-      </div>
-      <div class="mb-4 text-light">
-        <div class="description-container">
-          {{ product.beskrivning }}
-        </div>
-      </div>
-      <div class="border border-light mb-4"></div>
-
-      <div class="d-flex flex-row flex-lg-row justify-content-between">
-        <div class="text-light fw-bold mb-3">{{ product.pris }} kr</div>
-        <div class="d-flex">
-          <RouterLink
-            :to="'products/' + product.id"
-            class="h-100 hover-bg-primary text-white px-4 py-2 rounded text-center text-sm text-decoration-none fw-bold"
-            id="buy-button"
-          >
-            Välj
+  <table class="table table-dark m-0">
+    <thead class="">
+      <tr>
+        <th>Produkt</th>
+        <th>Pris</th>
+        <th>Ta bort från önskelista</th>
+      </tr>
+    </thead>
+    <tbody class="table-group-divider">
+      <tr v-for="product in wishList" :key="product.id" class="rounded shadow-md position-relative mb-3" id="bg">
+        <td class="d-flex align-items-center gap-2 border-0">
+          <img :src="product.bild" :alt="product.title" class="image-thumbnail d-none d-sm-block" style="width: 90px" />
+          <RouterLink :to="'products/' + product.id" class="text-white text-decoration-none">
+            <p class="m-0">{{ product.title }}</p>
           </RouterLink>
-          <button
-            class="btn btn-danger ms-2"
-            @click="removeFromWishList(product)"
-          >
-            Remove From Wishlist
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
+        </td>
+        <td class="align-middle border-0">
+          <p class="m-0">{{ product.pris }} kr</p>
+        </td>
+        <td class="align-middle border-0">
+          <button class="btn btn-danger" @click="removeFromWishList(product)">Ta bort från önskelistan</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
-
-<style scoped>
-.description-container {
-  height: 3rem;
-  overflow: hidden;
-  line-clamp: 2;
-  line-height: 1.5;
-}
-
-#bg {
-  background: #413d46;
-}
-
-#buy-button {
-  background: #212529;
-}
-
-#buy-button:hover {
-  background: #6b6f71;
-}
-
-.img-fluid {
-  width: 100%;
-  height: 200px;
-  object-fit: contain;
-}
-</style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -13,19 +13,21 @@ const user = ref(
   }
 );
 
-const orders = ref(JSON.parse(localStorage.getItem('orders') || '[]' ))
+const orders = ref(JSON.parse(localStorage.getItem("orders") || "[]"));
 function cancelOrder(id) {
-  const orderToCancel = orders.value.find((item) => item.id === id)
-  orderToCancel.orderStatus = "Avbruten"
-  localStorage.setItem('orders', JSON.stringify(orders.value))
+  const orderToCancel = orders.value.find((item) => item.id === id);
+  orderToCancel.orderStatus = "Avbruten";
+  localStorage.setItem("orders", JSON.stringify(orders.value));
 }
-
 
 onMounted(() => {
   localStorage.setItem("user", JSON.stringify(user.value));
 });
 
 const wishList = ref(JSON.parse(sessionStorage.getItem("wishList")) || []);
+const updateWishList = (newWishList) => {
+  wishList.value = newWishList;
+};
 const props = defineProps({
   activa: {
     type: Number,
@@ -88,30 +90,16 @@ const saveUserInfo = () => {
 
 <template>
   <section class="bg-dark">
-    <div
-      class="container p-4 mt-3 rounded shadow-sm"
-      style="background-color: #333333"
-    >
+    <div class="container p-4 mt-3 rounded shadow-sm" style="background-color: #333333">
       <div class="row">
         <div class="col-md-3 p-3 rounded" style="background-color: #333333">
-          <BFormRadioGroup
-            v-model="selected"
-            :options="options"
-            name="radios-btn-group1"
-            size="lg"
-            buttons
-            stacked
-          />
+          <BFormRadioGroup v-model="selected" :options="options" name="radios-btn-group1" size="lg" buttons stacked />
         </div>
         <div v-if="showOrders" class="col-md-9 text-light">
           <h1 class="h4 mb-3">Mina ordrar</h1>
           <p>Här hittar du samtliga beställningar.</p>
           <div class="mb-3">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Sök efter produktnamn eller ordernummer"
-            />
+            <input type="text" class="form-control" placeholder="Sök efter produktnamn eller ordernummer" />
           </div>
           <div v-if="orders.length > 0">
             <table class="table table-dark m-0">
@@ -126,7 +114,12 @@ const saveUserInfo = () => {
               <tbody class="table-group-divider">
                 <tr v-for="item in orders" :key="item.id" class="">
                   <td class="d-flex align-items-center gap-2 border-0">
-                    <img :src="item.bild" :alt="item.title" class="image-thumbnail d-none d-sm-block" style="width: 90px;">
+                    <img
+                      :src="item.bild"
+                      :alt="item.title"
+                      class="image-thumbnail d-none d-sm-block"
+                      style="width: 90px"
+                    />
                     <RouterLink :to="'products/' + item.id" class="text-white text-decoration-none">
                       <p class="m-0">{{ item.title }}</p>
                     </RouterLink>
@@ -137,8 +130,14 @@ const saveUserInfo = () => {
                   <td class="align-middle border-0">
                     <p class="m-0">{{ item.orderStatus }}</p>
                   </td>
-                  <td class="align-middle border-0" >
-                    <button class="btn btn-danger" @click="cancelOrder(item.id)" v-if="item.orderStatus === 'Levereras'">Avbeställ</button>
+                  <td class="align-middle border-0">
+                    <button
+                      class="btn btn-danger"
+                      @click="cancelOrder(item.id)"
+                      v-if="item.orderStatus === 'Levereras'"
+                    >
+                      Avbeställ
+                    </button>
                   </td>
                 </tr>
               </tbody>
@@ -158,41 +157,12 @@ const saveUserInfo = () => {
           <p>Här kan du ändra dina profilinställningar.</p>
           <div>
             <BFormGroup class="mb-3">
-              <BFormInput
-                class="mb-2"
-                type="text"
-                v-model="user.name"
-                placeholder="Namn"
-              />
-              <BFormInput
-                class="mb-2"
-                type="email"
-                v-model="user.email"
-                placeholder="E-postadress"
-              />
-              <BFormInput
-                class="mb-2"
-                type="tel"
-                v-model="user.phone"
-                placeholder="Telefonnummer"
-              />
-              <BFormInput
-                class="mb-2"
-                type="text"
-                v-model="user.address"
-                placeholder="Adress"
-              />
-              <BFormInput
-                class="mb-2"
-                type="password"
-                v-model="user.password"
-                placeholder="Nytt lösenord"
-              />
-              <BFormInput
-                class="mb-2"
-                type="password"
-                placeholder="Bekräfta nytt lösenord"
-              />
+              <BFormInput class="mb-2" type="text" v-model="user.name" placeholder="Namn" />
+              <BFormInput class="mb-2" type="email" v-model="user.email" placeholder="E-postadress" />
+              <BFormInput class="mb-2" type="tel" v-model="user.phone" placeholder="Telefonnummer" />
+              <BFormInput class="mb-2" type="text" v-model="user.address" placeholder="Adress" />
+              <BFormInput class="mb-2" type="password" v-model="user.password" placeholder="Nytt lösenord" />
+              <BFormInput class="mb-2" type="password" placeholder="Bekräfta nytt lösenord" />
             </BFormGroup>
             <BButton variant="primary" @click="saveUserInfo">Spara</BButton>
           </div>
@@ -201,7 +171,7 @@ const saveUserInfo = () => {
           <h1 class="h4 mb-3">Önskelista</h1>
           <p>Här kan du se din önskelista.</p>
           <div v-if="wishList.length > 0">
-            <WishList />
+            <WishList @updateWishList="updateWishList" />
           </div>
           <div v-else>
             <p class="fst-italic">Din önskelista är tom</p>
